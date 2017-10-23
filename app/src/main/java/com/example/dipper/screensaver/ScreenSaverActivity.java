aa5b1193417f0b41008a59046b39860c7ee39585
Dipper <yanliang.difference@gamil.com>
2017-10-23 15:10:57 +0800

    [基础]支持外部图片做展示

--- app/src/main/java/com/example/dipper/screensaver/ScreenSaverActivity.java
+++ app/src/main/java/com/example/dipper/screensaver/ScreenSaverActivity.java
@@ -1,14 +1,19 @@
 package com.example.dipper.screensaver;
 
+import android.Manifest;
 import android.app.Activity;
 import android.app.KeyguardManager;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.PowerManager;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 import android.view.WindowManager;
 
 
@@ -19,13 +24,11 @@ import android.view.WindowManager;
 public class ScreenSaverActivity extends Activity {
     public static final String TAG = "ScreenSaverActivity";
     public static final long time = 60000;
+    private static final int REQUEST_CODE = 1;
     private PagerContainer pagerContainer;
     private ViewPager viewPager;
     private MyHandler mHandler;
     private Handler handler;
-
-    private KeyguardManager km;
-    private KeyguardManager.KeyguardLock kl;
 
     private PowerManager pm;
     private PowerManager.WakeLock wl;
@@ -40,6 +43,8 @@ public class ScreenSaverActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+//        checkPermission();
+        Utils.makeImagesDir();
         initScreenSaver();
     }
 
@@ -78,4 +83,31 @@ public class ScreenSaverActivity extends Activity {
         }
         super.onDestroy();
     }
+    //取消掉申请权限、直接在源码中默认设置权限
+    /*private String[] permissions = {Manifest.permission.READ_EXTERNAL_STORAGE};
+
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.M) {
+            int i = ContextCompat.checkSelfPermission(this, permissions[0]);
+            if (i != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CODE);
+            } else {
+                Utils.makeImagesDir();
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_CODE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Utils.makeImagesDir();
+                }
+                break;
+            default:
+                break;
+        }
+    }*/
 }
